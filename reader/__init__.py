5ea0686df17ca015753c975582a08d9ad65f7efe
Geir Arne Hjelle <geirarne@gmail.com>
2018-09-16 21:03:31 +0200

    Add an example in __init__.py docstring

--- reader/__init__.py
+++ reader/__init__.py
@@ -1,6 +1,12 @@
 """Real Python feed reader
 
-asdf
+Import the `feed` module to work with the Real Python feed:
+
+    >>> from reader import feed
+    >>> feed.get_titles()
+    ['Logging in Python', 'The Best Python Books', ...]
+
+See https://github.com/realpython/reader/ for more information
 """
 # Version of realpython-reader package
 __version__ = "0.0.1"
