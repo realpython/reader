9c209b89e15cbc961081156463edc465cd817a66
Geir Arne Hjelle <geirarne@gmail.com>
2022-05-19 09:14:28 +0200

    Switch from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
-"""Setup script for realpython-reader"""
-
-# Standard library imports
-import pathlib
-
-# Third party imports
 from setuptools import setup
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).resolve().parent
-
-# The text of the README file is used as a description
-README = (HERE / "README.md").read_text()
-
-# This call to setup() does all the work
-setup(
-    name="realpython-reader",
-    version="1.0.0",
-    description="Read the latest Real Python tutorials",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/realpython/reader",
-    author="Real Python",
-    author_email="info@realpython.com",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-    ],
-    packages=["reader"],
-    include_package_data=True,
-    install_requires=["feedparser", "html2text"],
-    entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
-)
+setup()
